22ed3bb7e11e53a6d5485b7a01b6c95821eefb6b
MiHN <30841158+n-WN@users.noreply.github.com>
2022-04-03 11:54:02 +0800

    Delete grammer_demo directory

--- grammer_demo/bollean.java
+++
@@ -1,9 +0,0 @@
-package grammer_demo;
-
-public class bollean {
-    public static void main(String[] args) {
-        char a='g';
-        boolean b = a == 103;
-        System.out.println(b);
-    }
-}
